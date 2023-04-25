b1359ebc67ed35e1c18dcc65db8a47b182cdfbed
Afro <alex.fronteddu@pm.me>
2023-04-24 23:10:27 -0400

    fix output

--- ASN2IP.ps1
+++ ASN2IP.ps1
@@ -20,8 +20,9 @@ function Global:Get-ASNInfo {
         $response = Invoke-WebRequest -Uri $url -Method Get
     } catch {
         Write-Host "Request Error: Failed to retrieve information for $ORGANIZATION_NAME" -ForegroundColor Red
-        Write-Host "Status: $response.StatusCode"
-        Write-Host "Response: $(($response).Content)"
+        Write-Host "Status: ($response.StatusCode)"
+        Write-Host "Response: ($response.Content)"
+
         return
     }
     Write-Host "Getting AS Numbers........." -NoNewline -ForegroundColor Yellow
